031f37fa94290bd3a07d5dc439bd061b9023ca53
Moksleivis <moksleivis@KITM.local>
2019-11-15 09:31:53 +0200

    pataisyta

--- database/database.py
+++ database/database.py
@@ -11,7 +11,6 @@ def create_employee_table():
                                                 last_name text,
                                                 pay_roll integer
                                                 )""")
-    connection_cursor.execute("""INSERT  INTO employees (first_name, last_name, pay_roll) VALUES (Jonas, Aitis, 21)""")
     connection.commit()
     connection.close()
 
@@ -21,19 +20,43 @@ create_employee_table()
 
 def create_employee():
     connection = sqlite3.connect("employee.db")
-    connection.execute("""INSERT  INTO employees (first_name, last_name, pay_roll) VALUES (Jonas, Aitis, 21)""")
+    connection_cursor = connection.cursor()
+    connection_cursor.execute(
+        """INSERT  INTO employees (first_name, last_name, pay_roll) VALUES ('Jonas', 'Aitis', 21)""")
+    connection.commit()
+    connection.close()
+
+
+create_employee()
 
 
 def select_employee():
     connection = sqlite3.connect("employee.db")
-    connection.execute("""SELECT * FROM employees""")
+    connection_cursor = connection.cursor()
+    connection_cursor.execute("""SELECT * FROM employees""")
+    connection.close()
+
+
+select_employee()
 
 
 def update_employee():
     connection = sqlite3.connect("employee.db")
-    connection.execute("""UPDATE employees SET pay_roll = '30' WHERE id=1""")
+    connection_cursor = connection.cursor()
+    connection_cursor.execute("""UPDATE employees SET pay_roll = 30 WHERE id=1""")
+    connection.commit()
+    connection.close()
+
+
+update_employee()
 
 
 def delete_employee():
     connection = sqlite3.connect("employee.db")
-    connection.execute("""DELETE FROM employees WHERE id=1 LIMIT 1""")
+    connection_cursor = connection.cursor()
+    connection_cursor.execute("""DELETE FROM employees WHERE id=1""")
+    connection.commit()
+    connection.close()
+
+
+delete_employee()
